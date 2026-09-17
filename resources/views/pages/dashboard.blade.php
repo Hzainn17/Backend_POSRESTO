fb4e11f75d6fe495f0ce41137b6efc1cd4117964
Hilma Zain <hilmazain17@gmail.com>
2026-09-17 12:11:02 +0700

    remove deployment test

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -31,9 +31,6 @@
         <section class="section">
             <div class="section-header">
                 <h1>Dashboard</h1>
-                <span style="margin-left: 15px; font-size: 14px; color: #28a745;">
-                    CI/CD Deployment Test
-                </span>
             </div>
         </section>
     </div>
